b1a6ed2a255a114b36ae0d838bbe8f3fe343ee53
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2023-04-26 18:22:04 +0300

    tailwind

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,30 @@
 import React from "react";
+import linkedin from '../images/linkedin.svg'
+import github from '../images/github.svg'
+import twitter from '../images/twitter.svg'
 
 function Main() {
   return (
-    <div className="bg-gray-100 dark:bg-white">
-      <h1 className="text-black dark:text-white">GeeksforGeeks</h1>
+    <div className="flex">
+      <div className="flex flex-col">
+        <p>
+          Hey, I am Zeynep. I am a Computer Engineering student who loves
+          developing beautiful interfaces. In my free time I work on coding
+          projects, read and learn to design human-centered interfaces.
+        </p>
+
+        <div className="flex">
+          <button className="border-inherit border rounded-md">
+            View changelog
+          </button>
+
+          <img src={twitter} alt="twitter icon"></img>
+          <img src={linkedin} alt="linkedin icon"></img>
+          <img src={github} alt="github icon"></img>
+
+        </div>
+      </div>
+      <img></img>
     </div>
   );
 }
