83c002e1c287b7cc013c3bf569113dc89bc12d4f
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2023-04-27 19:33:07 +0300

    desktop done

--- src/pages/Writing.js
+++ src/pages/Writing.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import Table from "../components/Table";
 
 function Writing(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  const writing = [
+    {
+      name: "Do Things That Will Surpass Your Life",
+      time: "20 Apr 2021",
+      link: "/writing/surpass",
+    },
+    { name: "Boxing An AI", time: "24 Apr 2021", link: "/writing/decorating" },
+    {
+      name: "Everything I’ve learnt from decorating my tiny room",
+      time: "10 Mar 2023",
+      link: "/writing/decorating",
+    },
+  ];
+  return (
+    <div className="w-3/4 m-auto">
+      <h1 className="text-3xl text-[#582BE8] pb-8">writing.</h1>
+      <div className="flex flex-col gap-3">
+        {writing.map((w) => {
+          return <Table first={w.name} last={w.time} link={w.link}></Table>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Writing;
