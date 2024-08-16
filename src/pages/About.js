bedd0e137b2ff8fad33fba1ebe662e98f5ad5b49
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2024-08-16 07:47:32 +0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,11 +6,11 @@ function About() {
       <h1 className="text-3xl text-[#582BE8] pb-8 ">about.</h1>
       <p className="about">
 Hi, I’m Zeynep! I’m a UX/UI designer and front-end engineer with a background in Computer Science. My passion lies in creating accessible, user-centered technologies and crafting intuitive digital experiences. I’ve had the pleasure of working on a variety of projects, from sleek web apps to full-stack applications, always aiming to blend creativity with functionality.
-<br><br>
+<br></br>
 My tech career began in 2020, during the pandemic, when I had the opportunity to dive deep into coding and start building websites. The upside of everything being online was the chance to collaborate with amazing people from around the world.
-<br><br>
+<br></br>
 Currently, I’m working as a UX/UI Designer at Concide, where I focus on creating seamless user interfaces. Before this, I freelanced as a Web Developer on Contra and served as a Frontend Engineer at Oslo Philanthropic Exchange, where I gained valuable experience integrating databases and APIs. My first role was as a Web Developer at Brieferr, a startup where I learned the most by wearing many hats—conducting UX research, talking to users, and building a full-stack app using React and Node.js.
-<br><br>
+<br></br>
 When I’m not coding or designing, you can find me biking, reading interesting articles, or participating in fun coding projects. If you’d like to get in touch, feel free to <a href="mailto:zeynepevecen@gmail.com">email me</a> or reach out through my social media channels.      </p>
     </div>
   );
